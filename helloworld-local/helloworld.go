f1ef34d64bd8a1496606052ea4338fa237ac18bd
Pankaj Rai <prai@gotprint.com>
2017-09-20 16:19:43 +0530

    updated the helloworld-local

--- helloworld-local/helloworld.go
+++ helloworld-local/helloworld.go
@@ -1,12 +1,10 @@
-// Copyright 2015 Google Inc. All rights reserved.
-// Use of this source code is governed by the Apache 2.0
-// license that can be found in the LICENSE file.
-
-// Sample helloworld is a basic App Engine flexible app.
 package main
 
 import (
+	"encoding/json"
+	"flag"
 	"fmt"
+	"io/ioutil"
 	"log"
 	"math/rand"
 	"net/http"
@@ -17,28 +15,13 @@ import (
 	"golang.org/x/net/context"
 )
 
+var (
+	// flagPort is the open port the application listens on
+	flagPort = flag.String("port", "8000", "Port to listen on")
+)
+
 const PROJECT_ID string = "helloworld-180317"
 const KIND string = "helloworld"
-
-func main() {
-	http.HandleFunc("/", handle)
-	http.HandleFunc("/_ah/health", healthCheckHandler)
-	log.Print("Listening on port 8080")
-	log.Fatal(http.ListenAndServe(":8080", nil))
-}
-
-func handle(w http.ResponseWriter, r *http.Request) {
-	if r.URL.Path != "/" {
-		http.NotFound(w, r)
-		return
-	}
-	fmt.Fprint(w, "Hello world!")
-	SaveDataIntoDataStore(w, r)
-}
-
-func healthCheckHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "ok")
-}
 
 type Task struct {
 	Description string
@@ -46,7 +29,88 @@ type Task struct {
 	Age         int
 }
 
-func SaveDataIntoDataStore(w http.ResponseWriter, r *http.Request) {
+var tasks []Task
+
+func main() {
+
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", handler)
+
+	log.Printf("listening on port %s", *flagPort)
+	log.Fatal(http.ListenAndServe(":"+*flagPort, mux))
+}
+
+var results []string
+
+// GetHandler handles the index route
+func GetHandler(w http.ResponseWriter, r *http.Request) {
+
+	jsonBody, err := json.Marshal(tasks)
+	if err != nil {
+		http.Error(w, "Error converting results to json",
+			http.StatusInternalServerError)
+	}
+	w.Write(jsonBody)
+}
+
+// PostHandler converts post request body to string
+func handler(w http.ResponseWriter, r *http.Request) {
+
+	if r.Method == "POST" {
+
+		postHandler(w, r)
+	} else if r.Method == "GET" {
+		getsHandler(w, r)
+	} else {
+		http.Error(w, "Invalid request method"+r.RequestURI, http.StatusMethodNotAllowed)
+	}
+}
+
+func postHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+
+	var t Task
+	b, _ := ioutil.ReadAll(r.Body)
+	json.Unmarshal(b, &t)
+
+	tasks = append(tasks, t)
+	//set(task);
+
+	j, _ := json.Marshal(t)
+	w.Write(j)
+}
+
+func getsHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	queryValues := r.URL.Query()
+
+	t := Task{}
+	if len(queryValues.Get("Name")) > 0 {
+		t.Name = queryValues.Get("Name")
+	}
+
+	if len(queryValues.Get("Age")) > 0 {
+		age := queryValues.Get("Age")
+		t.Age, _ = strconv.Atoi(age)
+	}
+
+	if len(queryValues.Get("Description")) > 0 {
+		t.Description = queryValues.Get("Description")
+	}
+	if len(t.Name) > 0 {
+		tasks = append(tasks, t)
+		// set(t);
+	}
+
+	j, _ := json.Marshal(tasks) // get()
+	w.Write(j)
+}
+
+func healthCheckHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "ok")
+}
+
+func set(task Task) {
 	ctx := context.Background()
 
 	// Set your Google Cloud Platform project ID.
@@ -65,13 +129,6 @@ func SaveDataIntoDataStore(w http.ResponseWriter, r *http.Request) {
 	// Creates a Key instance.
 	taskKey := datastore.NameKey(kind, name, nil)
 
-	// Creates a Task instance.
-	task := Task{
-		Description: "datastore app",
-		Name:        "pankaj",
-		Age:         25,
-	}
-
 	// Saves the new entity.
 	if _, err := client.Put(ctx, taskKey, &task); err != nil {
 		log.Fatalf("Failed to save task: %v", err)
@@ -83,7 +140,31 @@ func SaveDataIntoDataStore(w http.ResponseWriter, r *http.Request) {
 	_, err = client.GetAll(ctx, q, &entities)
 
 	for _, v := range entities {
-		fmt.Fprintf(w, "[%s] %s, %d\n", v.Description, v.Name, v.Age)
+		fmt.Printf("[%s] %s, %d\n", v.Description, v.Name, v.Age)
 	}
+}
+
+func get() []Task {
+	ctx := context.Background()
+
+	// Set your Google Cloud Platform project ID.
+	projectID := PROJECT_ID
+
+	// Creates a client.
+	client, err := datastore.NewClient(ctx, projectID)
+	if err != nil {
+		log.Fatalf("Failed to create client: %v", err)
+	}
+
+	var entities []Task
+
+	q := datastore.NewQuery("helloworld")
+	_, err = client.GetAll(ctx, q, &entities)
+
+	for _, v := range entities {
+		fmt.Printf("[%s] %s, %d\n", v.Description, v.Name, v.Age)
+	}
+
+	return entities
 
 }
---
+++ helloworld-local/readme.txt
@@ -0,0 +1,9 @@
+Post Request:  
+	curl -d '{"Name":"Pankaj2", "Description": "user desc2", "Age" : "252"}' -i localhost:8000/
+
+get Request with query parameter: 
+	curl http://localhost:8000/?Name=Pankaj5&Age=252&Description=Desc4
+
+get Request: 
+	curl localhost:8000/
+
